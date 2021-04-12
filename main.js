0c452935e96c6f1836435712e54c7b76b8bcb1f6
acpastyear <82362238+acpastyear@users.noreply.github.com>
2021-04-12 21:51:03 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -63,23 +63,13 @@ function validateName() {
     return true;
 }
 
-function validateEmail() {
-    var name = document.getElementById('email').value;
-    if(!name.match(/^([A-Za-z\.\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        alert("Please enter your correct Email");
-        return false;
-    }
-
-    return true;
-}
-
 function resetForm(){
     document.getElementById('contact-form').reset();
     console.log('Reset');
 }
 
 function validateForm(){
-    if(!validateName() || !validateEmail()) {
+    if(!validateName()) {
         return false;
     }
 
